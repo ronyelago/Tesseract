4722690525baa1a084a68f33a28e6a670d8c3ef8
Ronye do Lago Cornetti Rocha <ronye.rocha@sistemasseguros.local>
2018-03-08 16:54:19 -0300

    adding files

---
+++ Mocr/Extractor/Extract.cs
@@ -0,0 +1,23 @@
+﻿using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+using System.Drawing;
+
+namespace Mocr.Extractor
+{
+    public class Extract
+    {
+        public static string ExtractTextFromImage(string path)
+        {
+            using (var api = OcrApi.Create())
+            {
+                api.Init(Languages.Portuguese);
+                using (var bmp = Bitmap.FromFile(path) as Bitmap)
+                {
+                    string plainText = api.GetTextFromImage(bmp);
+
+                    return plainText;
+                }
+            }
+        }
+    }
+}
---
+++ Mocr/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Mocr.Extractor;
+
+namespace Mocr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\ronye.rocha\Desktop\TravaLingua.jpg";
+            string text = Extract.ExtractTextFromImage(path);
+
+            foreach (var item in text)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n\n\n---Fim---\n");
+            Console.ReadLine();
+        }
+
+    }
+}
